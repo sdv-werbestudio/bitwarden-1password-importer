61d8a058a64e45d1fe1649921feea7749c7e5ec2
Robin Walloner <wallonerrobin@gmail.com>
2024-05-07 12:34:01 +0200

    Add authentication to script

--- migrate_personal_vault.sh
+++ migrate_personal_vault.sh
@@ -19,10 +19,52 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
 
+# Authenticate Bitwarden
+if ! bw login --check; then
+    clear
+    echo "Bitte authentifiziere dich bei Bitwarden:"
+    until bw login --apikey; do
+        echo ""
+    done
+    export BW_SESSION=""
+fi
+if ! bw unlock --check; then
+    clear
+    echo "Bitte entsperre deinen Bitwarden-Tresor:"
+    until export BW_SESSION=$(bw unlock --raw); do
+        echo ""
+    done
+fi
+
+# Authenticate 1Password
+until [[ $(op account list | wc -l) -ge 1 ]]; do
+    clear
+    echo "Bitte aktiviere die 1Password-CLI integration:"
+    echo ""
+    echo "  1. Öffne die 1Password-App"
+    echo "  2. Klicke oben links auf das Account-Menü"
+    echo "  3. Gehe zu 'Einstellungen' > 'Entwickler'"
+    echo "  4. Setze den Haken bei 'In 1Password-CLI integrieren'"
+    echo ""
+    read -p "Drücke Enter, wenn du fertig bist..."
+done
+clear
+if [[ $(op account list | wc -l) -ge 2 ]]; then
+    echo "Bitte wähle, welcher 1Password-Account verwendet werden soll:"
+    echo ""
+    op account list
+    echo ""
+    echo "Gib dazu die in der Spalte 'ID' stehende Nummer des Accounts ein."
+    read -p "USER ID: " ACCOUNT_ID
+else
+    ACCOUNT_ID=$(op account list | tail -n 1 | awk '{print $3}')
+fi
+
 # Run Python script
 read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
-python3 migrate_vault.py --dry-run null Employee
+python3 migrate_vault.py --dry-run null $ACCOUNT_ID Employee
 
 # Clean up
+bw lock
 deactivate
 rm -rf .venv
