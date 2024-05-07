531d430e44da7bc1d4a0173bb4b7ca43917941d1
Robin Walloner <wallonerrobin@gmail.com>
2024-05-07 11:41:54 +0200

    Clean up script

--- migrate_personal_vault.sh
+++ migrate_personal_vault.sh
@@ -7,31 +7,22 @@ read -p "Drücke Enter, um fortzufahren..."
 # Install Homebrew if not already installed
 if ! command -v brew &> /dev/null; then
     echo "Homebrew is not installed. Installing Homebrew..."
-    read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
-
-# Update Homebrew
-echo "Updating Homebrew..."
-read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
 brew update
 
 # Install Dependencies
-echo "Installing Python, Bitwarden CLI, and 1Password CLI..."
-read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
 brew install python bitwarden-cli 1password-cli
 
 # Install python dependencies
-echo "Installing Python dependencies..."
-read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
 
 # Run Python script
-echo "Running migration script..."
 read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
 python3 migrate_vault.py --dry-run null Employee
 
-echo "Script execution completed."
-read -p "Drücke Enter, um fortzufahren..." # TODO: Remove this line
+# Clean up
+deactivate
+rm -rf .venv
